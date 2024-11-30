fea03907cdc54e45eabf857199ca1190c8dd7b8a
Ziad Ibrahim <134080590+ZiadTantawy@users.noreply.github.com>
2024-11-30 15:29:48 +0200

    Fixed Public and Views Paths

--- Server/index.js
+++ Server/index.js
@@ -1,24 +1,32 @@
-import express from 'express';
-import session from 'express-session';
-import ejs from 'ejs';
-import connectToDB from './dbConnection.js';
+import express from "express";
+import path from "path";
+import connectToDB from "./dbConnection.js";
 
 const app = express();
 const Port = 3000;
+const __dirname = path.resolve(); // Resolve current directory
+
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(async (req, res, next) => {
     try {
         req.db = await connectToDB();
         next();
     } catch (err) {
+        console.error("Database connection error:", err);
         res.status(500).send("Failed to connect to the database.");
     }
 });
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Travel Guide!");
+    res.render("home");
 });
 
+
 app.listen(Port, () => {
-    console.log(`Server is running on port ${Port}`);
+    console.log(`Server is running on http://localhost:${Port}`);
 });
